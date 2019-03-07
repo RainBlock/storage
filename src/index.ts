7cf47b2b0893e5a2b94bc9bcf4a9b4d7ccc5cb92
SoujanyaPonnapalli <soujanyap95@gmail.com>
2019-03-06 19:34:58 -0600

    fix putGenesis

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import {toBufferBE} from 'bigint-buffer';
-import {BatchPut, Witness, MerklePatriciaTree, VerifyWitness} from '@rainblock/merkle-patricia-tree';
-import {hashAsBuffer, HashType} from 'bigint-hash';
 import {EthereumBlock} from '@rainblock/ethereum-block';
+import {BatchPut, MerklePatriciaTree, VerifyWitness, Witness} from '@rainblock/merkle-patricia-tree';
+import {toBufferBE} from 'bigint-buffer';
+import {hashAsBuffer, HashType} from 'bigint-hash';
 
 const ethHash = require('ethashjs');
 
 export interface Storage<K, V> {
   isEmpty: () => boolean;
   get: (key: K, root?: Buffer) => Witness;
-  putGenesis: (genesis: EthereumBlock, putOps: BatchPut[], delOps: K[]) => void;
+  putGenesis: (genesis: EthereumBlock, putOps: BatchPut[]) => void;
   update: (block: EthereumBlock, putOps: BatchPut[], delOps: K[]) => void;
   getBlockByNumber: (blockNum: number) => EthereumBlock;
   getBlockByHash: (hash: Buffer) => EthereumBlock;
@@ -28,34 +28,34 @@ export class StorageNode<K = Buffer, V = Buffer> implements Storage<K, V> {
 
   _blockNumberToHash = new Map();
 
-  _activeSnapshots : MerklePatriciaTree[] = [];
+  _activeSnapshots: MerklePatriciaTree[] = [];
 
   _gcThreshold = 256;
 
-  constructor (shard?: number, genesis?: EthereumBlock, putOps?: BatchPut[]) {
-    this._shard = (shard && shard >= 0 && shard < 16)? shard : -1;
+  constructor(shard?: number, genesis?: EthereumBlock, putOps?: BatchPut[]) {
+    this._shard = (shard && shard >= 0 && shard < 16) ? shard : -1;
     if (genesis && putOps) {
-      this.putGenesis(genesis, putOps, []);
+      this.putGenesis(genesis, putOps);
     }
   }
 
   private validateProofOfWork(block: EthereumBlock, root?: Buffer) {
     ethHash.verifyPOW(block, (valid: boolean) => {
       if (!valid) {
-        throw new Error("Invalid Block");
+        throw new Error('Invalid Block');
       }
-    })
+    });
     if (root) {
-      const blockStateRoot = hashAsBuffer(HashType.KECCAK256, toBufferBE(block.header.stateRoot, 32));
+      const blockStateRoot = hashAsBuffer(
+          HashType.KECCAK256, toBufferBE(block.header.stateRoot, 32));
       if (root.compare(blockStateRoot) !== 0) {
-        throw new Error("Block and State root mismatch")
-      }  
+        throw new Error('Block and State root mismatch');
+      }
     }
   }
 
-  isEmpty (): boolean {
-    if (this._blockchain.size !== 0 ||
-        this._rootMap.size !== 0 ||
+  isEmpty(): boolean {
+    if (this._blockchain.size !== 0 || this._rootMap.size !== 0 ||
         this._blockNumberToHash.size !== 0 ||
         this._activeSnapshots.length !== 0) {
       return false;
@@ -63,21 +63,21 @@ export class StorageNode<K = Buffer, V = Buffer> implements Storage<K, V> {
     return true;
   }
 
-  get (key: K, root?: Buffer): Witness {
+  get(key: K, root?: Buffer): Witness {
     let state;
     if (root && this._rootMap.has(root)) {
       const val = this._rootMap.get(root);
       state = val[1];
     } else {
       const len = this._activeSnapshots.length;
-      state = this._activeSnapshots[len -1];
+      state = this._activeSnapshots[len - 1];
     }
     return state.get(key);
   }
 
-  putGenesis (genesis: EthereumBlock, putOps: BatchPut[], delOps: K[]) {
+  putGenesis(genesis: EthereumBlock, putOps: BatchPut[]) {
     if (!this.isEmpty()) {
-      throw new Error("Invalid: putGenesis when Blockchain not empty")
+      throw new Error('Invalid: putGenesis when Blockchain not empty');
     }
 
     const trie = new MerklePatriciaTree();
@@ -93,16 +93,17 @@ export class StorageNode<K = Buffer, V = Buffer> implements Storage<K, V> {
     this._activeSnapshots.push(trie);
   }
 
-  private _deleteFirstInMap (map: Map<K, V>): any {
+  private _deleteFirstInMap(map: Map<K, V>): V|undefined {
     const keys = map.keys();
-    for (let key of keys) {
+    for (const key of keys) {
       const value = map.get(key);
-      map.delete(key)
+      map.delete(key);
       return value;
     }
+    return undefined;
   }
 
-  private gc () {
+  private gc() {
     if (this._activeSnapshots.length <= this._gcThreshold) {
       return;
     }
@@ -117,14 +118,13 @@ export class StorageNode<K = Buffer, V = Buffer> implements Storage<K, V> {
     }
   }
 
-  private persist (block: EthereumBlock, putOps: BatchPut[], delOps: K[]) {
-  }
+  private persist(block: EthereumBlock, putOps: BatchPut[], delOps: K[]) {}
 
-  private partitionKeys (putOps: BatchPut[], delOps: K[]) {
+  private partitionKeys(putOps: BatchPut[], delOps: K[]) {
     return [putOps, delOps];
   }
 
-  update (block: EthereumBlock, putOps: BatchPut[], delOps: K[]) {
+  update(block: EthereumBlock, putOps: BatchPut[], delOps: K[]) {
     this.gc();
     this.validateProofOfWork(block);
     const parentHash = block.header.parentHash;
@@ -132,7 +132,7 @@ export class StorageNode<K = Buffer, V = Buffer> implements Storage<K, V> {
     if (parentState) {
       parentState = parentState[1];
     } else {
-      throw new Error("Cannot find parent state");
+      throw new Error('Cannot find parent state');
     }
     const keys = this.partitionKeys(putOps, delOps);
     const trie = parentState.batchCOW(keys[0], keys[1]);
@@ -147,18 +147,18 @@ export class StorageNode<K = Buffer, V = Buffer> implements Storage<K, V> {
     this.persist(block, putOps, delOps);
   }
 
-  getBlockByNumber (blockNum: number): EthereumBlock {
+  getBlockByNumber(blockNum: number): EthereumBlock {
     const hash = this._blockNumberToHash.get(blockNum);
     return this.getBlockByHash(hash);
   }
 
-  getBlockByHash (hash: Buffer): EthereumBlock {
+  getBlockByHash(hash: Buffer): EthereumBlock {
     const value = this._blockchain.get(hash);
     return value[0];
   }
 
-  verifyWitness (root: Buffer, key: Buffer, witness: Witness): boolean {
-    VerifyWitness(root, key, witness)
+  verifyWitness(root: Buffer, key: Buffer, witness: Witness): boolean {
+    VerifyWitness(root, key, witness);
     return true;
   }
 }
