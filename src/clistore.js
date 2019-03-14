b1ea11b15b8c4e34565c927e3a2197a2e5f4f4a0
Aashaka Shah <aashaka96@gmail.com>
2019-03-14 18:13:27 -0500

    Add exception handling to each function

--- src/clistore.js
+++ src/clistore.js
@@ -12,15 +12,15 @@ const protoLoaderOptions = {
 };
 
 const packageDefinition =
-    protoLoader.loadSync(protoFileName, protoLoaderOptions);
+  protoLoader.loadSync(protoFileName, protoLoaderOptions);
 const proto = grpc.loadPackageDefinition(packageDefinition);
 const storageNode = new snode.StorageNode(-1, "../../src/test_data/genesis.json", "../../src/test_data/genesis.bin");
 
 const server = new grpc.Server();
-
+console.log("Started");
 function returnHi(hirequest) {
   let hiresponse = {
-    greeting: "Hi from server"
+  greeting: "Hi from server"
   };
   console.log(hirequest.greeting);
   return hiresponse;
@@ -31,65 +31,102 @@ function sayHi(call, callback) {
 }
 
 function GetCodeInfo(call) {
-    let resp = storageNode.getCode(call.request.address, call.request.codeOnly);
-    var existence = (resp.account.value === null ? false : true);
-    const proofreply = {
-        encoding: resp.account.proof
-    };
-    const witnessreply = {
-      value: resp.account.value,
-      proof: proofreply
-    };
-    const accreply = {
-        exists: existence,
-        witness: witnessreply
-    };
-    const codereply = {
-        accountInfo: accreply,
-        code: resp.code
-    };
-    return codereply;
+  console.log("Calling getCode");
+  console.log("Request is", call);
+  let resp;
+  try {
+    const address = Buffer.from(call.request.address.toString(),'hex');
+    resp = storageNode.getCode(address, call.request.codeOnly);
+  } catch (e) {
+    console.log("Error in codereply", e);
+    return e;
+  }
+  console.log("finished getCode call in server");
+  var existence = (resp.account.value === null ? false : true);
+  const proofreply = {
+    encoding: resp.account.proof
+  };
+  const witnessreply = {
+    value: resp.account.value,
+    proof: proofreply
+  };
+  const accreply = {
+    exists: existence,
+    witness: witnessreply
+  };
+  const codereply = {
+    accountInfo: accreply,
+    code: resp.code
+  };
+  console.log("CodeReply is: ", codereply);
+  return codereply;
 }
 
 function GetAccount(call) {
-    let resp = storageNode.get(call.request.address);
-    var existence = (resp.value === null ? false : true);
-    const proofreply = {
-        encoding: resp.proof
-    };
-    const witnessreply = {
-      value: resp.value,
-      proof: proofreply
-    };
-    const accreply = {
-        exists: existence,
-        witness: witnessreply
-    };
-    return accreply;
+  console.log("Received GetAccount call from client");
+  let resp;
+  try {
+    resp = storageNode.get(call.request.address);
+  }
+  catch (e) {
+    console.log("Error in getaccount", e);
+    return e;
+  }
+  var existence = (resp.value === null ? false : true);
+  const proofreply = {
+    encoding: resp.proof
+  };
+  const witnessreply = {
+    value: resp.value,
+    proof: proofreply
+  };
+  const accreply = {
+    exists: existence,
+    witness: witnessreply
+  };
+  console.log("getAccount successful in server. Sending to client:", accreply);
+  return accreply;
 }
 
 function GetStorage(call) {
-    let resp = storageNode.getStorage(call.request.address, call.request.key);
-
-    const proofreply = {
-        encoding: resp.proof
-    };
-    const witnessreply = {
-      value: resp.value,
-      proof: proofreply
-    };
-    const storagereply = {
-        witness: witnessreply
-    };
-    return storagereply;
+  console.log("Got GetStorage from client");
+  let resp;
+  try {
+    resp = storageNode.getStorage(call.request.address, call.request.key);
+  }
+  catch (e) {
+    console.log("Error in getstorage", e);
+    return e;
+  }
+  const proofreply = {
+    encoding: resp.proof
+  };
+  const witnessreply = {
+    value: resp.value,
+    proof: proofreply
+  };
+  const storagereply = {
+    witness: witnessreply
+  };
+  console.log("getStorage successful in server. Sending to client:", storagereply)
+  return storagereply;
 }
 
 function GetBlockHash(call) {
-    let resp = storageNode.getBlockHash(call.request.number);
-    const blockhashreply = {
-        hashes: resp
-    };
-    return blockhashreply;
+  console.log("GetBlockHash request from client");
+  let resp;
+  try {
+    resp = storageNode.getBlockHash(call.request.number);
+  }
+  catch (e) {
+    console.log("Error in getblockhash", e);
+    return e;
+  }
+  const blockhashreply = {
+    hashes: resp
+  };
+  console.log("getBlockHash successful in server. Sending to client:", blockhashreply)
+  return blockhashreply;
 }
 
 server.addService(proto.storageapi.StorageNode.service, {
