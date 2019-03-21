afbf3e1eeaacb69083d0dbb83f9c54a8c895442b
SoujanyaPonnapalli <soujanyap95@gmail.com>
2019-03-21 17:08:28 -0500

    Fix build error

--- src/testClient.ts
+++ src/testClient.ts
@@ -1,36 +1,35 @@
-import * as StorageNodeService from '../build/proto/clientStorage_grpc_pb';
-import {AccountReply, AccountRequest, BlockHashReply, BlockHashRequest, CodeReply, CodeRequest, MerklePatriciaTreeNode, RPCWitness, StorageReply, StorageRequest} from '../build/proto/clientStorage_pb';
 import * as grpc from 'grpc';
-import {rlpToEthereumAccount} from './utils';
-import {RlpDecode, RlpList} from 'rlp-stream';
+import * as StorageNodeService from '../build/proto/clientStorage_grpc_pb';
 
-const runServer = (port: string) => {
-  var client = new StorageNodeService.StorageNodeClient('localhost:50051',
-                                       grpc.credentials.createInsecure());
-  var request = new CodeRequest();
-  const addr = Buffer.from("000d836201318ec6899a67540690382780743280", 'hex');
+import {AccountReply, AccountRequest, BlockHashReply, BlockHashRequest, CodeReply, CodeRequest, MerklePatriciaTreeNode, RPCWitness, StorageReply, StorageRequest} from '../build/proto/clientStorage_pb';
+
+const runTestClient = (port: string) => {
+  const client = new StorageNodeService.StorageNodeClient(
+      'localhost:50051', grpc.credentials.createInsecure());
+  const request = new CodeRequest();
+  const addr = Buffer.from('000d836201318ec6899a67540690382780743280', 'hex');
   request.setAddress(addr);
   request.setCodeOnly(false);
-  console.log("REACHED");
-  client.getCodeInfo(request, function(err, response) {
-  	const accountInfo = response.getAccountInfo();
-  	const rlpaccount = accountInfo!.getWitness()!.getValue();
-//   	const account = rlpToEthereumAccount(RlpDecode(rlpaccount as Buffer) as RlpList);
-  	console.log("Received codeinfo: ", rlpaccount);
+  console.log('REACHED');
+  client.getCodeInfo(request, (err, response) => {
+    const accountInfo = response.getAccountInfo();
+    const rlpaccount = accountInfo!.getWitness()!.getValue();
+    console.log('Received codeinfo: ', rlpaccount);
   });
 };
 
 const printUsage = () => {
-	console.log("Usage");
-}
+  console.log('USAGE: node -r ts-node/register src/testClient.ts port');
+  process.exit(-1);
+};
 
-const callServer = () => {
+const callClient = () => {
   if (process.argv.length !== 3) {
     printUsage();
   }
   const port = process.argv[2];
-  console.log('Started client: ');
-  runServer(port);
+  console.log('Starting client on port', port);
+  runTestClient(port);
 };
 
-callServer();
+callClient();
