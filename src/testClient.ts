fd6e00a21d12dd22b562f20961ed6c2b83b04e20
SoujanyaPonnapalli <soujanyap95@gmail.com>
2019-03-21 17:32:41 -0500

    Test clientRPC getAccount

--- src/testClient.ts
+++ src/testClient.ts
@@ -1,21 +1,54 @@
 import * as grpc from 'grpc';
-import * as StorageNodeService from '../build/proto/clientStorage_grpc_pb';
+import {RlpDecode, RlpList} from 'rlp-stream/build/src/rlp-stream';
 
+import * as StorageNodeService from '../build/proto/clientStorage_grpc_pb';
 import {AccountReply, AccountRequest, BlockHashReply, BlockHashRequest, CodeReply, CodeRequest, MerklePatriciaTreeNode, RPCWitness, StorageReply, StorageRequest} from '../build/proto/clientStorage_pb';
+
+import {EthereumAccount, rlpToEthereumAccount} from './utils';
+
+const assertEquals = (n0: bigint, n1: bigint) => {
+  if (n0 !== n1) {
+    throw new Error(
+        'AssertionError!' +
+        '\nReceived: ' + n0.toString() + '\nExpected: ' + n1.toString());
+  }
+};
+
+const testGetCodeInfo = (client: StorageNodeService.StorageNodeClient) => {
+  const request = new CodeRequest();
+  const addr = Buffer.from('000d836201318ec6899a67540690382780743280', 'hex');
+  const correctAccount: EthereumAccount = {
+    balance: 200000000000000000000n,
+    nonce: 0n,
+    storageRoot:
+        39309028074332508661983559455579427211983204215636056653337583610388178777121n,
+    codeHash:
+        89477152217924674838424037953991966239322087453347756267410168184682657981552n,
+  };
+  request.setAddress(addr);
+  request.setCodeOnly(false);
+  client.getCodeInfo(request, (err, response) => {
+    if (err) {
+      console.log('ERROR: getCodeInfo\n', err);
+      return;
+    }
+    const accountInfo = response.getAccountInfo();
+    const rlpaccount = Buffer.from(accountInfo!.getWitness()!.getValue_asU8());
+    const account = rlpToEthereumAccount(RlpDecode(rlpaccount) as RlpList);
+
+    assertEquals(account.balance, correctAccount.balance);
+    assertEquals(account.nonce, correctAccount.nonce);
+    assertEquals(account.codeHash, correctAccount.codeHash);
+    assertEquals(account.storageRoot, correctAccount.storageRoot);
+    console.log('Received CodeInfo:\n', account);
+  });
+};
 
 const runTestClient = (port: string) => {
   const client = new StorageNodeService.StorageNodeClient(
-      'localhost:50051', grpc.credentials.createInsecure());
-  const request = new CodeRequest();
-  const addr = Buffer.from('000d836201318ec6899a67540690382780743280', 'hex');
-  request.setAddress(addr);
-  request.setCodeOnly(false);
-  console.log('REACHED');
-  client.getCodeInfo(request, (err, response) => {
-    const accountInfo = response.getAccountInfo();
-    const rlpaccount = accountInfo!.getWitness()!.getValue();
-    console.log('Received codeinfo: ', rlpaccount);
-  });
+      'localhost:' + port, grpc.credentials.createInsecure());
+
+  testGetCodeInfo(client);
 };
 
 const printUsage = () => {
